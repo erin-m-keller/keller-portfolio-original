57e0dd6884b4346b956eca3bb814b94318595764
Erin Keller <erinkeller@Erins-MacBook-Pro-2.local>
2023-03-29 10:46:21 -0400

    pushing gh pages

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Main from "./pages/Main";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        {/* main content displays here */}
         <Main />
     </React.StrictMode>
 );
