bfb2cf7443e8e6d51b5aff6f3a78f64565b0c252
Erin Keller <aestheticartist@gmail.com>
2023-06-28 12:21:08 -0400

    updating build

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import Main from "./pages/Main";
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* main content displays here */}
-        <Main />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Main />
+        </BrowserRouter>
     </React.StrictMode>
 );
